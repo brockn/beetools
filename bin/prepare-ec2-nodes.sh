a5baeb608f4116bd8cb2ae2b0813ac176b2250a5
Brock Noland <brock@apache.org>
2014-03-16 18:23:26 -0500

    Format drives in parallel

--- bin/prepare-ec2-nodes.sh
+++ bin/prepare-ec2-nodes.sh
@@ -48,12 +48,23 @@ run service ntpd start
 run chkconfig ntpd on
 
 # Format all storage volumes except the root volume
-run mkfs -t ext4 /dev/xvdf
-run mkfs -t ext4 /dev/xvdg
+tmpdir=/tmp/mkfs-$(date +%s)
+mkdir -p $tmpdir
+run mkfs -t ext4 /dev/xvdf 1>$tmpdir/xvdf.out 2>$tmpdir/xvdf.err </dev/null &
+run mkfs -t ext4 /dev/xvdg 1>$tmpdir/xvdg.out 2>$tmpdir/xvdg.err </dev/null &
 data_devices=$(lsblk | egrep '^xvd[a-z]+ ' | awk '!$7 {print $1}' | egrep -v 'xvdf|xvdg')
+rm -rf /tmp/mkfs-
 for dev in $data_devices
 do
-  run mkfs -t ext4 -m 1 -T largefile -O dir_index,extent,sparse_super /dev/$dev
+  run mkfs -t ext4 -m 1 -T largefile -O dir_index,extent,sparse_super /dev/$dev  1>$tmpdir/$dev.out 2>$tmpdir/$dev.err </dev/null &
+done
+
+wait
+
+for file in $tmpdir/*
+do
+  echo $file
+  cat $file
 done
 
 # Rename existing /var/log dir
